f1266a01eb826889ae48a2877305b9ae7590def7
Rafael Brum <raf.brum@pm.me>
2021-02-17 19:19:00 +0000

    lesson 04

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
+import Footer from "../src/components/commons/Footer";
 import Menu from "../src/components/commons/Menu";
 
 export default function Home() {
   return (
-    <Menu />
+    <div style={{
+      flex: '1',
+      display: 'flex',
+      flexWrap: 'wrap',
+      flexDirection: 'column',
+      justifyContent: 'space-between'
+    }}>
+      <Menu />
+      <Footer />
+    </div>
   )
 }
