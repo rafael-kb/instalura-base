de78967427eeea60b4aa4961548d73019a023a21
Rafael Brum <raf.brum@pm.me>
2021-03-08 21:37:57 +0000

    lesson 03 module 02 - sign up form

--- src/components/foundation/layout/Grid/index.js
+++ src/components/foundation/layout/Grid/index.js
@@ -38,6 +38,10 @@ const Row = styled.div`
   flex-wrap: wrap;
   margin-right: -16px;
   margin-left: -16px;
+  ${propToStyle('flex')}
+  ${propToStyle('marginLeft')}
+  ${propToStyle('marginRight')}
+  ${propToStyle('justifyContent')}
 `;
 
 const Col = styled.div`
@@ -51,6 +55,7 @@ const Col = styled.div`
   ${propToStyle('alignItems')};
   ${propToStyle('justifyContent')};
   ${propToStyle('flexDirection')};
+  ${propToStyle('paddingRight')}
 
   ${({ value }) => {
     if (typeof value === 'number') {
